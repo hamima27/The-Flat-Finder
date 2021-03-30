1d326cec0bc7fae08f04adea1ff2a7bb06aee9ca
hamima27 <60136574+hamima27@users.noreply.github.com>
2021-03-30 16:02:34 +0100

    login button not working

--- src/FormSuccess.js
+++ src/FormSuccess.js
@@ -5,7 +5,6 @@ const FormSuccess = () => {
   return (
     <div className="form-content-right">
       <div className="form-success">We have received your registration!</div>
-      <img src="https://i.imgur.com/9Ka9mEE.png" alt="success-image" />
     </div>
   );
 };
