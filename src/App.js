f50e1379568bc26b6ec9f2839a59aa501a396931
PSBrar <60395218+PSBrar@users.noreply.github.com>
2021-04-02 19:05:46 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import "./App.css";
 import React from "react";
 import Form from "./Form";
+import Login from "./loginForm";
+//import Hello1 from "./hello";
+import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
 
 export default function App() {
   return (
     <div className="App">
-      <Form />
+      <Router>
+        <Switch>
+          <Route path="/" exact component={Form} />
+          <Route path="/Login" exact component={Login} />
+        </Switch>
+      </Router>
     </div>
   );
 }
